317c25b5b74234c98426e897035c3ec3e3fa938e
Felix Gerberding <felix@opossumts.net>
2020-01-16 23:59:30 +0100

    microservice basics

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+
+// to validate date
+Date.prototype.isValid = function() {
+  return this.getTime() === this.getTime(); // NaN === NaN --> false
+};
+
+// Express Middleware
+app.use(cors({ optionSuccessStatus: 200 }));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/timestamp/:date_string", function(req, res) {
+  var d = new Date(req.params.date_string);
+
+  if (d.isValid()) {
+    res.json({ unix: d.getTime(), utc: d.toUTCString() });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+// Starting the server obviously
+app.listen(process.env.PORT);
